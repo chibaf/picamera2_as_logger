eb74c61a584b92547a203e36b5151b2eb99c8d63
chibaf <chibaf@mac.com>
2024-03-22 17:05:37 +0900

    Add files via upload

--- picam5b.py
+++ picam5b.py
@@ -1,22 +1,18 @@
 from picamera2 import Picamera2, Preview
 from libcamera import controls
 import time
+from datetime import date
 
 pc2 = Picamera2()
 pc2.start_preview( True )
-
-i=0
-
 while True:
-
-  i=i+1
-
-# start??????set_controls??????
+  today = date.today()
+  t=time.localtime()
+  current_time=time.strftime("_H%H_M%M_S%S",t)
+  fn="photo_"+str(today)+current_time+".jpg"
   pc2.start()
-
-# Continuous???
-#  ?????????????????????
   pc2.set_controls( {"AfMode" : controls.AfModeEnum.Continuous} )
-  pc2.capture_file("test"+str(i)+".jpg")
-
   time.sleep( 5 )
+  pc2.capture_file(fn)
+  time.sleep(5)
+
